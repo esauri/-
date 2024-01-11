b013522ebec248d3ec71e891550bed5c63db82bf
Erick Sauri <ericksauri@hey.com>
2024-01-10 21:06:48 -0500

    logo

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
-// const blog = defineCollection({
-//   type: "content",
-//   // Type-check frontmatter using a schema
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     // Transform string to Date object
-//     pubDate: z.coerce.date(),
-//     updatedDate: z.coerce.date().optional(),
-//     heroImage: z.string().optional(),
-//   }),
-// });
+const blog = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
 
 /**
  * Miscellaneous content, usually these are imported individually such as `/about`
