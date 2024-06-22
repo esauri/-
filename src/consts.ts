5a96815afb038f649e27fe18486092cd2cf88ad3
Erick Sauri <ericksauri@hey.com>
2024-06-22 18:13:18 -0400

    chore(): add resume to project

--- src/consts.ts
+++ src/consts.ts
@@ -9,6 +9,5 @@ export const PERSONAL_LINKS = {
   email: "mailto:ericksaurisyl@gmail.com",
   github: "https://github.com/esauri",
   linkedIn: "https://www.linkedin.com/in/erick-sauri-1847357b",
-  resume:
-    "https://firebasestorage.googleapis.com/v0/b/portfolio-7ad56.appspot.com/o/media%2FErick%20Sauri%20Resume.pdf?alt=media&token=9e8a8332-65fd-4982-a82d-fbe4eff862c5",
+  resume: "/Erick-Sauri-Resume.pdf",
 };
